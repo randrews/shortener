1173fb871f403c4fde64b0009cc4c912dd5b5b43
Ross Andrews <ross.andrews@gmail.com>
2015-04-30 20:42:37 -0500

    A couple more tests implemented

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -8,8 +8,18 @@ RSpec.describe Link, type: :model do
     expect(l.key).to be_present
   end
 
-  it "should look up values for keys"
-  it "should accept typo'd keys"
+  it "should look up values for keys" do
+    l = Link.create(url: "http://google.com", key: 'ABCDE')
+
+    expect( Link.for_key('ABCDE').first ).to be_present
+  end
+
+  it "should accept typo'd keys" do
+    l = Link.create(url: "http://google.com", key: '1010')
+
+    expect( Link.for_key('IOlo').first ).to be_present
+  end
+
   it "should reject keys with bad words"
 
 end
